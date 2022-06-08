1d13b721de58337fced1f3327ce4fd435e842832
Rumdidum <waldemar.rink2021@gmail.com>
2022-06-08 23:24:31 +0200

    add eventlistener for textinput

--- scripts/etch_script.js
+++ scripts/etch_script.js
@@ -1,15 +1,44 @@
 let div = document.querySelector('#canvas');
 let input = document.querySelector('input');
-let output = document.querySelector('output');
+let output = document.querySelector('#output');
 let value = input.getAttribute('value');
 let newValue;
-output.textContent = input.value;
-drawRectangles();
+
+function checkTextInput() {
+    output.addEventListener('keypress', logKey);
+
+    function logKey(e) {
+        if (e.key === "Enter") {
+            
+            newValue = output.value;
+            input.setAttribute("value", newValue);
+            div.innerHTML = ''
+            if (parseInt(newValue) <= 64 && typeof parseInt(newValue) === 'number' && parseInt(newValue) >= 16) {
+                drawRectangles(newValue);
+                console.log(typeof parseInt(newValue));
+            } else if (parseInt(newValue) >= 64) {
+                alert("Number is to high!")
+                drawRectangles();
+            } else if (parseInt(newValue) <= 16) {
+                alert("Number is to low!")
+                drawRectangles();
+            }
+                
+        } else {
+            return
+        }
+    }
+}
+checkTextInput()
+
+
 input.addEventListener('input', () => {
     //  This one is important to delete old squares
     div.innerHTML = ''
     //   
-    output.textContent = input.value;
+
+    // updates output value
+    output.value = input.value;
     newValue = input.value;
     input.setAttribute("value", newValue);
     drawRectangles(newValue);
@@ -28,7 +57,7 @@ function drawRectangles(newV = value) {
         };
     };
 }
-
+drawRectangles();
 // TODO erstelle eine einfache Zeichnung mit mehreren divs in einem Container und versuche ihre Größe nach dem Container anzupassen.
 // TODO Ich sollte auch eine Zahl mit der Hand eingeben können
 // TODO Ich sollte vorherig erstelle DiVs löschen
