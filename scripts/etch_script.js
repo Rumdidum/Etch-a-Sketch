52cfb49be81583d91dca14553bdf39b1d1530791
Rumdidum <waldemar.rink2021@gmail.com>
2022-06-08 22:05:52 +0200

    Added my script from codepen when i could not use my pc

--- scripts/etch_script.js
+++ scripts/etch_script.js
@@ -1,13 +1,37 @@
-const canvas = document.querySelector('#canvas');
-const firstLoop = document.createElement('div');
-const secondLoop = document.createElement('div');
+let div = document.querySelector('#canvas');
+let input = document.querySelector('input');
+let output = document.querySelector('output');
+let value = input.getAttribute('value');
+let newValue;
+output.textContent = input.value;
+drawRectangles();
+input.addEventListener('input', () => {
+    //  This one is important to delete old squares
+    div.innerHTML = ''
+    //   
+    output.textContent = input.value;
+    newValue = input.value;
+    input.setAttribute("value", newValue);
+    drawRectangles(newValue);
+}, true);
 
-firstLoop.style.width = "20px";
-firstLoop.style.height = "20px";
-// firstLoop.style.backgroundColor = "gray";
-canvas.appendChild(firstLoop)
-canvas.appendChild(firstLoop)
-firstLoop.style.border = "solid 1px blue";
-for(let i = 0; i < 16; i++) {
-    canvas.appendChild(firstLoop);
+function drawRectangles(newV = value) {
+    for (let i = 0; i < newV; i++) {
+        let squares = document.createElement('div');
+        squares.classList.add('item0');
+        //      These should be empty containers
+        div.appendChild(squares);
+        for (let j = 0; j < newV; j++) {
+            let secondLoop = document.createElement('div');
+            secondLoop.classList.add('item1');
+            squares.appendChild(secondLoop);
+        };
+    };
 }
+
+// TODO erstelle eine einfache Zeichnung mit mehreren divs in einem Container und versuche ihre Größe nach dem Container anzupassen.
+// TODO Ich sollte auch eine Zahl mit der Hand eingeben können
+// TODO Ich sollte vorherig erstelle DiVs löschen
+// TODO Größe der Squares bei einer höheren Zahl kleiner werden lassen
+// und an die größe des Containers anpassen. (vieleicht geht das ja auch mit Flexbox)
+// Ich denke mit height und width komme ich nicht weiter da sie flexbox aushebeln und ich somit 
